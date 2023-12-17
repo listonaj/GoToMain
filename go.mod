a7e503537794f800e27c0b5ea1c81437fc3b903c
Jean-Marc Romain <90714406+listonaj@users.noreply.github.com>
2023-12-16 16:17:32 -0800

    test main function

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/listonaj/GoToMain
 
 go 1.21.5
 
-require (
-	github.com/listonaj/GoToDog v0.0.0-20231216201417-d5f07d2e2b11 // indirect
-	github.com/listonaj/puppy v0.0.0-20231216203153-2dd68151ef5f // indirect
-)
+require github.com/listonaj/puppy v0.0.0-20231216212525-981486ec2f46
+
+require github.com/listonaj/GoToDog v0.0.0-20231216201417-d5f07d2e2b11 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	dog "github.com/listonaj/GoToDog"
 	"github.com/listonaj/puppy"
 )
 
@@ -10,5 +11,19 @@ func main() {
 	fmt.Println("HelloWorld")
 	s1 := puppy.Bark()
 	fmt.Println(s1)
+
+	s2 := puppy.BigBark()
+
+	fmt.Println(s2)
+
+	s3 := puppy.Barks()
+
+	fmt.Println(s3)
+
+	s4 := dog.WhenGrownUp(s2)
+	s5 := dog.WhenGrownUp(s3)
+
+	fmt.Println(s4)
+	fmt.Println(s5)
 
 }
